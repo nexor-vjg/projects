5f9e04a0f2ea5cb66b69062d2e3ab1a9fc1275db
Nexor <codingvi364@gmail.com>
2026-01-08 12:31:56 +0200

    Update guessnumber.py

--- guessnumber.py
+++ guessnumber.py
@@ -1,11 +1,11 @@
 import random
 numbers=['1','2','3','4','5']
-gamerunning=True
-while gamerunning :
+
+while True :
     if input("Guess Numbers from 1 to 5 : ")==random.choice(numbers):
         print("You're right")
     else :
         print("wrong answer") 
     if input("Do you want to play again ? (y/n)") == "n":
-        gamerunning=False 
+        break 
         
